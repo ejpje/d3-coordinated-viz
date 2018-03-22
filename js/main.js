97a42b4fa6f4dd72835c950b0088db7569fbbf8c
ejpje <emilypettit@Emilys-MacBook-Pro-1-24-17.local>
2018-03-22 10:40:46 -0500

    Trying to join csv and geojson data layers

--- js/main.js
+++ js/main.js
@@ -19,24 +19,48 @@ function setMap(){ //set up choropleth map
     .scale(600)
     .translate([width / 2, height / 2]);
 
-  var path = d3.geoPath() //reate path generator to draw the geographies
+  var path = d3.geoPath() //create path generator to draw the geographies
     .projection(projection);
 
   d3.queue() //queue to parallelize asynchronous data loading
     .defer(d3.csv, "data/d3data.csv") //load attributes from csv
-    .defer(d3.json, "data/USA_adm1.topojson") //load background spatial data
+    .defer(d3.json, "data/USAstates.topojson") //load background spatial data
     .await(callback);
 
   function callback(error, csvData, usa){
-    var usaStates = topojson.feature(usa, usa.objects.USA_adm1).features; //convert topojson to geojson
+    var continentalUSA = topojson.feature(usa, usa.objects.USAstates).features; //convert topojson to geojson
+
+    var attrArray = ["State", "FID", "2016medHouseInc", "USFAclubs", "USFAmembers", "2017tournaments", "RioOlympians", "LondonOlympians"]; //variables for data join
+
+    for (var i=0; i<csvData.length; i++){ //loop through csv to assign each set of csv attribute values to geojson state
+      var csvState = csvData[i]; //the current state
+      var csvKey = csvState.FID; //the csv primary key
+      console.log(csvKey);
+      for (var a=0; a<continentalUSA.length; a++){ //loop through geojson states to find correct state
+
+        var geojsonProps = continentalUSA[a].properties; //the current state geojson properties
+        var geojsonKey = geojsonProps.FID; //the geojson primary key
+
+        if (geojsonKey == csvKey){ //where primary keys match, transfer csv data to geojson properties object
+
+          attrArray.forEach(function(attr){ //assign all attributes and values
+            var val = parseFloat(csvState[attr]); //get csv attribute value
+            geojsonProps[attr] = val; //assign attribute and value to geojson properties
+          });
+        };
+      };
+    };
 
     var states = map.selectAll(".states") //add states to the map
-      .data(usaStates)
+      .data(continentalUSA)
       .enter()
       .append("path")
       .attr("class", function(d){
         return "states " + d.properties.State;
       })
       .attr("d", path);
-  };
+
+      console.log(csvData);
+      console.log(continentalUSA);
+    };
 };
